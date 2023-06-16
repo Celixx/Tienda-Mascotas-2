1ad5ba77d8272ab9278d373d2995027a134c2290
Celix <alonsomurua@gmail.com>
2023-06-16 19:52:38 -0400

    no se ayuda

--- TiendaMascotas/core/urls.py
+++ TiendaMascotas/core/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import index,misdatos
+from .views import index,misdatos, nosotros, comprasAnteriores, ingreso
 
 
 urlpatterns = [
     path('', index, name="index"),
     path('misdatos', misdatos, name='misdatos'),
+    path('nosotros', nosotros, name='nosotros'),
+    path('comprasAnteriores', comprasAnteriores, name='comprasAnteriores'),
+    path('ingreso', ingreso, name='ingreso'),
 ]
     # path('poblar_bd', poblar_bd, name="poblar_bd"),
     # path('vehiculo/<action>/<id>', vehiculo, name="vehiculo"),
--- TiendaMascotas/core/views.py
+++ TiendaMascotas/core/views.py
@@ -6,4 +6,13 @@ def index(request):
 def misdatos(request):
     return render(request, 'core/misdatos.html')
 
+def nosotros(request):
+    return render(request, 'core/nosotros.html')
+
+def comprasAnteriores(request):
+    return render(request, 'core/comprasAnteriores.html')
+
+def ingreso(request):
+    return render(request, 'core/ingreso.html')
+
 # Create your views here.
