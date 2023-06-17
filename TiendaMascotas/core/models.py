cc39048d5d75027b4c7c07e3ad69a2a6bc6dcb84
Celix <alonsomurua@gmail.com>
2023-06-17 16:28:58 -0400

    cambio git ignore y cambio models

--- TiendaMascotas/core/models.py
+++ TiendaMascotas/core/models.py
@@ -3,7 +3,9 @@ from django.db import models
 class Producto(models.Model):
     idProducto = models.IntegerField(primary_key=True, verbose_name="ID de producto")
     Categoria = models.CharField(max_length=10, blank=False, null=False, verbose_name="Categoría")
+    Nombre = models.CharField(max_length=50, blank=False, null=False, verbose_name="Nombre Producto")
     Precio = models.IntegerField(blank=False, null=False, verbose_name="Precio")
+    Descripcion = models.CharField(max_length=300, blank=False, null=False, verbose_name="Descripción")
     DescuentoSubscriptor = models.IntegerField(blank=False, null=False, verbose_name="Descuento Subscriptor %")
     DescuentoOferta = models.IntegerField(blank=False, null=False, verbose_name="Descuento por Oferta")
 
